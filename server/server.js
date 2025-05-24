3697448dff581f156a9092eea202992a7f9cf5a4
ChauhanShivang <chauhanshivang2003@gmail.com>
2025-05-24 15:28:01 +0530

    next commit

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import { connectDB } from "./lib/db.js"
 import userRouter from "./routes/userRoutes.js"
 import messageRouter from "./routes/messageRoutes.js"
 import {Server} from "socket.io"
-
+//
 import serverless from "serverless-http"
 
 // create express app using HTTP server
